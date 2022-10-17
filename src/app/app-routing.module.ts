743ba05974fd29fe51e8bc630058d77e670f8c0c
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-10-17 23:29:44 +0300

    did comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ let routes:Routes = [
   {path: '', component: HomeComponent},
   {path:'users', loadChildren:() => import('./modules/users/users.module').then(m =>m.UsersModule)},
   {path:'posts', loadChildren:() => import('./modules/posts/posts.module').then(m => m.PostsModule)},
-  {path:'comments', loadChildren:() => import('./modules/comments/comments.module').then(m => m.CommentsModule)}
+  {path:'comments', loadChildren:() => import('./modules/comments/comments.module').then(m => m.CommentsModule)},
+  {path:'albums', loadChildren:() => import('./modules/comments/comments.module').then(m => m.CommentsModule)}
 ];
 
 @NgModule({
