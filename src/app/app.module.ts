d6667922361792357d8e3617c7a6c5179298626c
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-10-31 19:13:09 +0200

    lesson6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/constants/urls.ts
@@ -0,0 +1,8 @@
+import {environment} from "../../environments/environment";
+
+
+const {API:baseURL} = environment;
+
+export const urls = {
+  users: `${baseURL}/users`
+}
